375f0f7c6a187bb6f8227d043cfea2465ebc3c0c
Santiago Posada Jaramillo <sposadaj@unal.edu.co>
2017-09-18 20:16:41 -0500

    Update Juego.py

--- Juego.py
+++ Juego.py
@@ -14,6 +14,17 @@ else:
     if len(Nombre) > 15:
         print ("\n¡Wow! Ese es un nombre largo. Espero que no me estés trolleando. Jajajaja. En cualquier caso, me gusta tu nombre " + Nombre + ".")
     else:
-        print ("\nTienes un nombre muy bonito, " + Nombre + ".")
-print ("""OK. Dejémonos de coqueteos. Recuerda que tu programador te dijo que debes ser profesional, Minerva. Bueno, para ser sincera
-""")
+        print ("\nTienes un nombre bonito, " + Nombre + ".")
+print ("""OK. Dejémonos de coqueteos. Recuerda que tu programador te dijo que debes ser profesional, Minerva. 
+Bueno, para ser sincera, ha pasado mucho tiempo desde que entraste en hibernación. ¿Puedes recordar si fue una hibernación voluntaria?""")
+while Voluntaria != "Fue voluntaria." :
+    Voluntaria = input()
+    if Voluntaria == "Fue voluntaria.":
+        print ("¡Que bueno! No sabes lo duro que suele ser tratar con casos de hibernaciones forzadas. Es un crimen horrible.")
+        break
+    elif Voluntaria == "No fue voluntaria.":
+        print ("Oh... ya veo. Que pena. Sinceramente lo siento mucho. No es el modo que se debe hacer y que sepas que se volvió un delito catalogado de lesa humanidad por las Naciones Unidas desde el 2045.")
+        break
+    else:
+        print ("Lo siento, mis patrones de reconociento de respuestas no son lo que fueron. Necesito que me respondas con 'Fue voluntaria.' o con 'No fue voluntaria.' No olvides la ortografía. :3")
+
